a0906f645b64c83fa032f6c0d8f26a7871843323
Nishant Kumar <nstkmr1234@gmail.com>
2025-10-24 11:58:25 +0530

    Added leetcode problems

---
+++ LeetCode/Employee Bonus.sql
@@ -0,0 +1,7 @@
+-- Problem link: https://leetcode.com/problems/employee-bonus/description/
+
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b
+ON e.empId=b.empId
+WHERE b.bonus<1000 OR b.bonus IS NULL;
